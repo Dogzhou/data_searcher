5370bc10aeea16927c3bff3bc3048f9799225198
Edward Zhou <Edward_mjz@hotmail.com>
2019-07-15 19:23:06 +1000

    Init Project

---
+++ lib/data_searcher.ex
@@ -0,0 +1,18 @@
+defmodule DataSearcher do
+  @moduledoc """
+  Documentation for DataSearcher.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DataSearcher.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/data_searcher_test.exs
@@ -0,0 +1,8 @@
+defmodule DataSearcherTest do
+  use ExUnit.Case
+  doctest DataSearcher
+
+  test "greets the world" do
+    assert DataSearcher.hello() == :world
+  end
+end
